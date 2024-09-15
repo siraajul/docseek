a852e63fc5219327f0eef0155b64fc68a3ee68db
siraajul <shirajulislamparvez@gmail.com>
2024-09-15 23:59:10 +0600

    Add: -> Padding & CrossAxAl

--- lib/home.dart
+++ lib/home.dart
@@ -10,14 +10,23 @@ class HomePage extends StatelessWidget {
       body: Container(
         color: const Color(0xff51A8FF),
         height: 350,
+        padding: const EdgeInsets.all(16),
         width: double.infinity,
         child: const Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               'Hi Edi',
-              style: TextStyle(fontSize: 16),
-            )
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            Text(
+              "Let's find\nyour top doctor",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w600),
+            ),
           ],
         ),
       ),
