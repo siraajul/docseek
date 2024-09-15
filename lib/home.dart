9546b1d3c58a794f25851fe695117e3807d0836a
siraajul <shirajulislamparvez@gmail.com>
2024-09-16 00:31:44 +0600

    Import: Header Method

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ui/home/header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,62 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        color: const Color(0xff51A8FF),
-        height: 350,
-        padding: const EdgeInsets.all(16),
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //Right Text
-                const Text(
-                  'Hi Edi',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-                //Right Icon
-                Container(
-                  height: 45,
-                  width: 45,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text(
-              "Let's find\nyour top doctor!",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            //------------------------SearchBar---------------------------------
-            const TextField(
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Search Here....',
-                  hintStyle: TextStyle(fontWeight: FontWeight.w400),
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(Radius.circular(50)))),
-            ),
-          ],
-        ),
-      ),
+      body: header(),
     );
   }
 }
