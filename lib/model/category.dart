f6621139e3756428abde74b00db81ea676277705
siraajul <shirajulislamparvez@gmail.com>
2024-09-16 10:22:48 +0600

    Model: Category

---
+++ lib/model/category.dart
@@ -0,0 +1,23 @@
+class CategoryModel {
+  String vector;
+  bool isSelected;
+
+  CategoryModel({
+    required this.vector,
+    required this.isSelected,
+  });
+
+  static List<CategoryModel> getCategories() {
+    List<CategoryModel> categories = [];
+
+    categories.add(
+        CategoryModel(vector: 'assets/vectors/heart.svg', isSelected: false));
+    categories.add(
+        CategoryModel(vector: 'assets/vectors/pil.svg', isSelected: false));
+    categories.add(
+        CategoryModel(vector: 'assets/vectors/dentist.svg', isSelected: false));
+    categories.add(
+        CategoryModel(vector: 'assets/vectors/pregnant.svg', isSelected: false));
+    return categories;
+  }
+}
